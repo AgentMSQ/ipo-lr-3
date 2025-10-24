64735da379021d5c425afd4b2bf8ce84ed52d3fa
Фригидная сука <6agentmsq9@gmail.com>
2025-10-24 16:05:22 +0300

    вааааааааау это оч круто

--- task_3/main.py
+++ task_3/main.py
@@ -1,12 +1,12 @@
-day = int(input("Введите день: "))
-month = int(input("Введите месяц: "))
-if (month == 12 and day >= 1 and day <= 31) or (month == 1 and day >= 1 and day <= 30) or (month == 2 and day >= 1 and day <= 28):
-    print("Зима")
-elif (month == 3 and day >= 1 and day <= 30) or (month == 4 and day >= 1 and day <= 31) or (month == 5 and day >= 1 and day <= 30):
-    print("Весна")
-elif (month == 6 and day >= 1 and day <= 31) or (month == 7 and day >= 1 and day <= 30) or (month == 8 and day >= 1 and day <= 31):
-    print("Лето")
-elif (month == 9 and day >= 1 and day <= 30) or (month == 10 and day >= 1 and day <= 31) or (month == 11 and day >= 1 and day <= 30):
-    print("Осень")
+day = int(input("Введите день: ")) #ввод дня 
+month = int(input("Введите месяц: ")) #ввод месяца
+if (month == 12 and day >= 1 and day <= 31) or (month == 1 and day >= 1 and day <= 30) or (month == 2 and day >= 1 and day <= 28): #условия зимы, чек месяца и дня
+    print("Зима") #вывод зимы
+elif (month == 3 and day >= 1 and day <= 30) or (month == 4 and day >= 1 and day <= 31) or (month == 5 and day >= 1 and day <= 30): #условия весны, чек месяца и дня
+    print("Весна") #вывод весны
+elif (month == 6 and day >= 1 and day <= 31) or (month == 7 and day >= 1 and day <= 30) or (month == 8 and day >= 1 and day <= 31): #условия лета, чек месяца и дня
+    print("Лето") #вывод лета
+elif (month == 9 and day >= 1 and day <= 30) or (month == 10 and day >= 1 and day <= 31) or (month == 11 and day >= 1 and day <= 30): #условия осени, чек месяца и дня
+    print("Осень") #вывод осени
 else:
-    print("Введена неверная дата")
+    print("Введена неверная дата") #вывод неверной даты, если ошибка в введенных данных
